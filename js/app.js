8a551e7d2a927ec4befefd8136f7356c98a645c6
LongLH <d@dapps.me>
2017-07-21 23:48:13 +0700

    improve load time

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
-$(window).ready(function() {
-	var placeholder = $('#media');
-	var preview = $('#media-preview');
+// $(window).ready(function() {
+// 	var placeholder = $('#media');
+// 	var preview = $('#media-preview');
 
-	var imgLarge = new Image();
-	imgLarge.src = placeholder.data('large');
-	imgLarge.onload = function () {
-		$(imgLarge).addClass('loaded');
-	};
+// 	var imgLarge = new Image();
+// 	imgLarge.src = placeholder.data('large');
+// 	imgLarge.onload = function () {
+// 		$(imgLarge).addClass('loaded');
+// 	};
 
-	placeholder.append(imgLarge);
-});
+// 	placeholder.append(imgLarge);
+// });
+
+$('#media').one('load', function() {
+	$(this).addClass('loaded');
+}).each(function() {
+	if(this.complete) {
+		$(this).load();
+	}
+});;
