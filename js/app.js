cd5dd4ddd34bcac4a0b0dcfe57e8cd26535357bd
LongLH <d@dapps.me>
2017-08-07 15:11:40 +0700

    fix GChrome audit comments

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
-// $(window).ready(function() {
-// 	var placeholder = $('#media');
-// 	var preview = $('#media-preview');
-
-// 	var imgLarge = new Image();
-// 	imgLarge.src = placeholder.data('large');
-// 	imgLarge.onload = function () {
-// 		$(imgLarge).addClass('loaded');
-// 	};
-
-// 	placeholder.append(imgLarge);
-// });
-
 $('#media').one('load', function() {
 	$(this).addClass('loaded');
 }).each(function() {
